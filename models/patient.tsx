d23ff437b2e9174efbb3a01ce5a94ff2344c17bf
VasilisPapakonstantinou <v.papakonstantinou@ysoft.gr>
2024-09-12 10:28:01 +0300

    New commit

--- models/patient.tsx
+++ models/patient.tsx
@@ -1,10 +1,18 @@
 import mongoose from 'mongoose';
+import autoIncrement from "mongoose-auto-increment";
 
 const NewPatientSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    patronimo: { type: String },
-    amka: { type: String, required: true, unique: true, length: 11},
-}, { timestamps: true });
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    patronimo: {type: String},
+    amka: {type: String, required: true, unique: true, length: 11},
+}, {timestamps: true});
+
+NewPatientSchema.plugin(autoIncrement.plugin, {
+    model: 'NewPatient',
+    field: 'id',
+    startAt: 1,
+    incrementBy: 1
+})
 
 export default mongoose.models.NewPatient || mongoose.model('NewPatient', NewPatientSchema);
