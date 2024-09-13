d24c09fa1c4ca2352b9dee2f3c9ab0c9c47d506f
VasilisPapakonstantinou <v.papakonstantinou@ysoft.gr>
2024-09-13 17:14:04 +0300

    New commit

--- models/patient.tsx
+++ models/patient.tsx
@@ -1,18 +1,34 @@
 import mongoose from 'mongoose';
-import autoIncrement from "mongoose-auto-increment";
 
 const NewPatientSchema = new mongoose.Schema({
+    id: { type: Number, unique: true },
     name: {type: String, required: true},
     surname: {type: String, required: true},
     patronimo: {type: String},
     amka: {type: String, required: true, unique: true, length: 11},
 }, {timestamps: true});
 
-NewPatientSchema.plugin(autoIncrement.plugin, {
-    model: 'NewPatient',
-    field: 'id',
-    startAt: 1,
-    incrementBy: 1
-})
+
+// Pre-save hook to auto-increment the `id` before saving the document
+NewPatientSchema.pre('save', async function (next) {
+    const doc = this;
+
+    // Check if this is a new document
+    if (doc.isNew) {
+        try {
+            // Find the highest `id` in the current collection
+            const highestIdDoc = await mongoose.models.NewPatient.findOne({}, {}, { sort: { id: -1 } });
+
+            // If a document exists, increment the ID by 1, otherwise start at 1
+            doc.id = highestIdDoc ? highestIdDoc.id + 1 : 1;
+
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();  // Proceed for updates, no increment needed
+    }
+});
 
 export default mongoose.models.NewPatient || mongoose.model('NewPatient', NewPatientSchema);
