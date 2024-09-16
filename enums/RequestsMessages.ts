aaec9fea318e5f644895c5f7dfdfbe5235dc7fab
VasilisPapakonstantinou <v.papakonstantinou@ysoft.gr>
2024-09-16 10:22:47 +0300

    New commit

---
+++ enums/RequestsMessages.ts
@@ -0,0 +1,8 @@
+enum RequestsMessages {
+    NEW_PATIENT = 'Οι πληροφορίες του ασθενή αποθηκεύτηκαν επιτυχώς.',
+    NEW_PATIENT_ERROR = 'Σφάλμα κατά την αποθήκευση νέου ασθενή',
+    NEW_EXAM =   'Οι πληροφορίες της εξέτασης αποθηκεύτηκαν επιτυχώς',
+    NEW_EXAM_ERROR = 'Σφάλμα κατά την αποθήκευση νέας εξέτασης'
+}
+
+export default RequestsMessages
